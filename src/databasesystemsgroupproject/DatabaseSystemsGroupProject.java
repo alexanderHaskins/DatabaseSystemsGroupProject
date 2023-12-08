4a8e40319f2acc1931abcc2c2bbb815c5cd08ef3
alexanderHaskins <alexjhaskins@yahoo.com>
2023-12-08 15:37:39 -0600

    Tried to add the code from the example; however, I encountered an error where I could not load the driver which I could not fix despite my best efforts. I will try again later to fix this

--- src/databasesystemsgroupproject/DatabaseSystemsGroupProject.java
+++ src/databasesystemsgroupproject/DatabaseSystemsGroupProject.java
@@ -3,18 +3,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package databasesystemsgroupproject;
+import java.sql.*;
+import java.util.*;
+
 
 /**
  *
  * @author alexj
  */
 public class DatabaseSystemsGroupProject {
-
+    //Dummy variables for testing purposes
+    private static String url="jdbc:postgresql://localhost:5432/ClassExampleCompanyManagmentDB";
+    private static String user="postgres";
+    private static String password="S@n0cgaP5";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected");
+
+            
+
+            
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Cannot load driver.");
+        } catch (SQLException e) {
+            System.out.println("SQL error");
+        }
     }
     
 }
